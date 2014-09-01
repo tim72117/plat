1edfdabe593601ef0130e17f55155440bd1a9caf
tim cai <tim72117@yahoo.com.tw>
2014-09-01 22:16:02 +0800

    demo: add table creater

--- app/views/demo/use/page/table.php
+++ app/views/demo/use/page/table.php
@@ -1,19 +1,29 @@
 
-<script src="../js/angular.min.js"></script>
+<!--<script src="../js/angular.min.js"></script>-->
 
 <div ng-app="app">
 
-    <div ng-controller="newTableController" ng-switch on="page" style="border: 0px solid #999;position: absolute;top: 10px;bottom: 0">
+    <div ng-controller="newTableController" ng-switch on="page" style="border: 0px solid #999;position: absolute;top: 10px;bottom: 0">        
+        
+        <div ng-switch-when="1" style="border: 1px solid #999;position: absolute;top: 0;bottom: 40px;width:800px;overflow: scroll">            
+            
+            <div class="column" style="width: 30px;left: 2px;top: 2px"></div>   
+            <div class="column" ng-repeat="table in tables" style="width: 80px;left: {{ ($index+1)*79-48 }}px;top:2px;padding-left:2px">{{ table.name }}</div> 
+            <div ng-repeat="($rindex, row) in rows | startFrom:(page-1)*limit | limitTo:limit">
+                <div class="column" style="width: 30px;left: 2px;top:{{ ($rindex+1)*29+2 }}px;text-align: center">{{ $rindex+1 }}</div>   
+                <div class="column" ng-repeat="table in tables" style="width: 80px;left: {{ ($index+1)*79-48 }}px;top:{{ ($rindex+1)*29+2 }}px;padding-left:2px" contenteditable="true">row</div>
+            </div>
+        </div>
 
-        <div ng-switch-when="1" style="border: 1px solid #999;position: absolute;top: 0;bottom: 40px;width:800px">
+        <div ng-switch-when="2" style="border: 1px solid #999;position: absolute;top: 0;bottom: 40px;width:800px">
             <div ng-repeat="table in tables" style="margin:2px">
-                <input type="text" placeholder="欄位名稱" class="input" value="{{ table.name }}" />
-                <input type="text" placeholder="欄位描述" class="input" value="{{ table.description }}" />
+                <input type="text" placeholder="欄位名稱" class="input" ng-model="table.name" autofocus="{{table.autofocus || 'false'}}" />
+                <input type="text" placeholder="欄位描述" class="input" ng-model="table.description" />
                 <select class="input"><option>欄位類型</option></select>
                 <select class="input"><option>過濾規則</option></select>
             </div>    
             <div style="margin:2px">
-                <input type="text" placeholder="欄位名稱" class="input" ng-model="column.name" />
+                <input type="text" placeholder="欄位名稱" class="input" ng-model="column.name" ng-change="add()" />
                 <input type="text" placeholder="欄位描述" class="input" ng-model="column.description" />
                 <select class="input"><option>欄位類型</option></select>
                 <select class="input"><option>過濾規則</option></select>        
@@ -21,18 +31,9 @@
             </div>   
         </div>
         
-        <div ng-switch-when="2" style="border: 1px solid #999;position: absolute;top: 0;bottom: 40px;width:800px">
-            <div ng-repeat="table in tables" style="margin:2px">
-                <input type="text" placeholder="欄位名稱" class="input" value="{{ table.name }}" />
-                <input type="text" placeholder="欄位描述" class="input" value="{{ table.description }}" />
-                <select class="input"><option>欄位類型</option></select>
-                <select class="input"><option>過濾規則</option></select>
-            </div>    
-        </div>
-        
         <div style="height:40px;border-top: 1px solid #999;position: absolute;bottom: 0">
             <div class="page-tag" ng-click="page=1" ng-class="page==1 ? 'selected' : ''" style="margin:0 0 5px 5px;">資料表</div>
-            <div class="page-tag" ng-click="page=2" ng-class="page==2 ? 'selected' : ''" style="margin:0 0 5px 5px;left:85px">資料表</div>
+            <div class="page-tag" ng-click="page=2" ng-class="page==2 ? 'selected' : ''" style="margin:0 0 5px 5px;left:85px">欄位定義</div>
         </div>
         
     </div>
@@ -42,6 +43,15 @@
 </div>
 
 <style>
+.column {
+    box-sizing: border-box;
+    position: absolute;
+    border: 1px solid #999;
+    font-size: 13px;
+    line-height: 30px;
+    height: 30px;
+    overflow: hidden
+}    
 .page-tag {
     position: absolute;
     top: -1px;
@@ -87,6 +97,8 @@ angular.module('app', [])
 }).controller('Ctrl', Ctrl).controller('newTableController', newTableController);
 
 function newTableController($scope) {
+    $scope.page = 1;
+    $scope.limit = 20;
     $scope.tables = [
         {
             name: '姓名',
@@ -95,9 +107,17 @@ function newTableController($scope) {
         {
             name: '姓名',
             description : '姓名'
+        },
+        {
+            name: '身分證',
+            description : '身分證'
         }
     ];
     $scope.column = {};
+    $scope.rows = [];
+    for(i=1;i<10000;i++){
+        $scope.rows.push(i);
+    }
     $scope.add = function() {
         $scope.tables.push({
             name: $scope.column.name,
@@ -105,6 +125,8 @@ function newTableController($scope) {
         });
         $scope.column.name = null;
         $scope.column.description = null;
+        $scope.tables[$scope.tables.length-1].autofocus = 'false';
+        console.log($scope.tables[$scope.tables.length-1]);
     };
     $scope.page = 1;
     $scope.pageC = function() {
