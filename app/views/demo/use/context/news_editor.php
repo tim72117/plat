637d5cf7ad711175447aa9cfa1452be2ad563012
tim cai <tim72117@yahoo.com.tw>
2015-10-05 13:29:44 +0800

    dev: add news editor

--- app/views/demo/use/context/news_editor.php
+++ app/views/demo/use/context/news_editor.php
@@ -1,60 +1,120 @@
-<div ng-controller="newsController">
-    <div style="display:inline-block;width:100px">標題</div>
-    <div style="display:inline-block;width:500px;margin-left:20px">內文</div>
-    <div style="display:inline-block;width:100px;margin-left:20px">發布時間</div>
-    <div ng-repeat="anews in news">
-        <div style="display:inline-block;width:100px;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;color:blue;cursor: pointer" ng-click="edit(anews)">{{ anews.title }}</div>
-        <div style="display:inline-block;width:500px;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;margin-left:20px">{{ anews.context }}</div>
-        <div style="display:inline-block;width:100px;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;margin-left:20px">{{ anews.publish_at }}</div>
-    </div>    
-    <div>
-        <input type="hidden" ng-model="newID" ng-init="newID=null" />
-        <input type="text" ng-model="newTitle" style="padding:5px;width:500px" placeholder="標題" />
-    </div>
-    <div style="margin: 2px 0 0 0;line-height:30px;height:30px">發布日期<input type="date" ng-model="newPublish" style="line-height:25px;height:25px;box-sizing: border-box" /></div>
-    <div style="margin: 2px 0 0 0">
-        <textarea ng-model="newContext" rows="15" style="width:500px"></textarea>
-    </div>
+<div ng-cloak ng-controller="newsController" style="position: absolute;top: 10px;bottom: 10px;left: 10px; right: 10px;max-width:800px">
     
-    <div class="file-btn" ng-click="editNews()" ng-class="{hover:!saving, disabled:saving}" style="width:80px;height:25px;line-height:25px;background-color: #eee;font-size:14px;float:left">
-        <div ng-show="!newID">新增</div>
-        <div ng-show="newID">儲存</div>
+    <div class="ui segment">
+        
+        <div class="fluid ui basic mini button news" ng-click="newsAdd()">
+            <i class="file outline icon"></i>
+            新增
+        </div>
+
+        <div class="ui divided list">
+            <div class="item" ng-repeat="anews in news | orderBy:['!deleted_at', 'publish_at']:true" ng-class="{disabled: anews.deleted_at}">
+                <button class="ui red icon mini button right floated" ng-class="{disabled: anews.deleted_at}" ng-click="delete(anews)"><i class="trash outline icon"></i> 刪除</button>
+                <div class="ui toggle checkbox right floated" ng-click="setDisplay(anews)">
+                    <input type="checkbox" ng-model="anews.display_at.intro" ng-disabled="anews.disabled">
+                    <label>公告於首頁</label>
+                </div>
+                <i class="announcement icon left "></i>            
+                <div class="content">
+                    <a class="header news" ng-click="newsEdit(anews, $event)">{{ anews.title }}</a>
+                    {{ anews.context }}
+                    <div class="description"> {{ anews.publish_at }}</div>
+
+                </div>
+            </div>
+        </div> 
+
+        <div class="ui flowing popup" id="newsEditor">   
+            <form class="ui form" style="width:500px">
+                <div class="field">
+                    <label>標題</label>
+                    <input type="text" ng-model="newTitle" placeholder="標題" />
+                    <input type="hidden" ng-model="newID" ng-init="newID=null" />
+                </div>
+                <div class="field">
+                    <label>發布日期</label>
+                    <input type="date" ng-model="newPublish" placeholder="發布日期" />
+                </div>    
+                <div class="field">
+                    <label>內文</label>
+                    <textarea ng-model="newContext" rows="15"></textarea>
+                </div>  
+                <div class="ui positive button" ng-click="editNews()" ng-class="{loading: saving}">儲存</div> 
+            </form>
+        </div>
+        
     </div>
+        
 </div>
 <script>
 angular.module('app')
-.controller('newsController', newsController);
-
-function newsController($scope, $http, $filter) {
+.controller('newsController', function($scope, $http, $filter, $timeout) {
     
     $scope.saving = false;
     $scope.news = [];
     
-    $scope.edit = function(anew) {
+    $scope.newsAdd = function(anew, event) {
+        $scope.newID = null;
+        $scope.newTitle = null;
+        $scope.newContext = null;
+        $scope.newPublish = null;
+        $scope.saving = false;
+    };
+    
+    $scope.newsEdit = function(anew, event) {
         $scope.newID = anew.id;
         $scope.newTitle = anew.title;
         $scope.newContext = anew.context;
-        $scope.newPublish = anew.publish_at;
+        $scope.newPublish = new Date(anew.publish_at);   
+        $scope.saving = false;
+    };
+    
+    $scope.setDisplay = function(anews) {        
+        anews.display_at.intro = anews.display_at.intro || false;
+        if( anews.disabled )
+            return false;
+        
+        var anews_ = angular.copy(anews);
+        anews_.display_at.intro=!anews_.display_at.intro;
+
+        anews.disabled = true;
+
+        $http({method: 'POST', url: 'ajax/setDisplay', data:{id: anews.id, display_at: anews_.display_at} })
+        .success(function(data, status, headers, config) { 
+            anews.display_at = data.display_at;
+            anews.disabled = false;
+        }).error(function(e){
+            console.log(e);
+        });
     };
     
     $scope.editNews = function() {
         if( $scope.saving )
             return false;
-
+        //var anews = $filter('filter')($scope.news, {id: data.new.id}, function(actual, expected) { return angular.equals(actual, expected); })[0];
         $scope.saving = true;    
-        $http({method: 'POST', url: 'ajax/saveNews', data:{id: $scope.newID, title: $scope.newTitle, context: $scope.newContext, publish_at: $scope.newPublish} })
+        $http({method: 'POST', url: 'ajax/saveNews', data:{id: $scope.newID, title: $scope.newTitle, context: btoa(encodeURIComponent(angular.toJson($scope.newContext))), publish_at: $scope.newPublish} })
         .success(function(data, status, headers, config) { 
+            data.new.display_at = angular.fromJson(data.new.display_at) || {};
             if( data.method==='insert' ){
                 $scope.news.push(data.new);
             }
             if( data.method==='update' ){
                 angular.extend($filter('filter')($scope.news, {id: data.new.id}, function(actual, expected) { return angular.equals(actual, expected); })[0], data.new);
             }
-            $scope.newID = null;
-            $scope.newTitle = null;
-            $scope.newContext = null;
-            $scope.newPublish = null;
-            $scope.saving = false;
+            $('.news').popup('hide');
+        }).error(function(e){
+            console.log(e);
+        });
+    };
+
+    $scope.delete = function(news_old) {
+        $http({method: 'POST', url: 'ajax/delete', data:{id: news_old.id} })
+        .success(function(data, status, headers, config) { 
+            if (data.news) {
+                data.news.display_at = angular.fromJson(data.news.display_at) || {};
+                angular.extend(news_old, data.news);
+            };
         }).error(function(e){
             console.log(e);
         });
@@ -62,30 +122,30 @@ function newsController($scope, $http, $filter) {
     
     $http({method: 'POST', url: 'ajax/getNews', data:{} })
     .success(function(data, status, headers, config) { 
+        
+        angular.forEach(data, function(anews, key) {
+            anews.display_at = angular.fromJson(anews.display_at) || {};
+        });
+
         $scope.news = data;
+
+        $timeout(function() {
+            $('.news').popup({
+                popup: $('#newsEditor'),
+                position: 'bottom left',
+                on: 'click'
+            });  
+        });
+
     }).error(function(e){
         console.log(e);
     });
-}
+    
+});
 </script>
+
+<script src="/css/ui/Semantic-UI-1.11.1/components/popup.js"></script>
+
 <style>
-.file-btn {
-    cursor: pointer; 
-    text-align: center;
-    border: 1px solid #aaa;
-    color:#555;
-    -webkit-touch-callout: none;
-    -webkit-user-select: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-}
-.file-btn.hover:hover {
-    border: 1px solid #888;
-    color:#000;
-}
-.file-btn.disabled {
-    color:#aaa;
-}
+
 </style>
--- app/views/demo/use/controller/news_editor.php
+++ app/views/demo/use/controller/news_editor.php
@@ -1,31 +1,45 @@
 <?
 return [
     'getNews' => function() {
-        return $news = DB::table('news')->where('project', 1)->get();
+        return $news = DB::table('news')->where('project', 1)->orderBy('publish_at', 'desc')->get();
     },
     'saveNews' => function() {
         if( is_null(Input::get('id')) ) {
+			$context = json_decode(urldecode(base64_decode(Input::get('context'))));
             $id = DB::table('news')->insertGetId([
                 'project' => 1,
                 'title' => Input::get('title'),
-                'context' => Input::get('context'),
+                'context' => $context,
                 'publish_at' => Input::get('publish_at'),
                 'created_by' => Auth::user()->id,
-                'created_at' => date("Y-n-d H:i:s"),
+                'created_at' => Carbon\Carbon::now()->toDateTimeString(),
             ]);
             $method = 'insert';
         }else{
             $id = Input::get('id');
+			$context = json_decode(urldecode(base64_decode(Input::get('context'))));
             DB::table('news')->where('id', Input::get('id'))->update([
                 'project' => 1,
                 'title' => Input::get('title'),
-                'context' => Input::get('context'),
+                'context' => $context,
                 'publish_at' => Input::get('publish_at'),
-                'updated_at' => date("Y-n-d H:i:s"),
+                'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
             ]);
             $method = 'update';
         }
         $new = DB::table('news')->where('id', $id)->first();
         return ['new' => $new, 'method' => $method];
+    },
+    'setDisplay' => function() {
+        if( !is_null(Input::get('id')) && isset(Input::get('display_at')['intro']) && in_array(Input::get('display_at')['intro'], [true, false]) ) {
+            DB::table('news')->where('id', Input::get('id'))->update(['display_at'=>json_encode(Input::get('display_at'))]);
+        }
+        return ['display_at'=>Input::get('display_at')];
+    },
+    'delete' => function() {
+        DB::table('news')->where('project', 1)->where('id', Input::get('id'))->update(['deleted_at' => Carbon\Carbon::now()->toDateTimeString()]);
+        return [
+            'news' => DB::table('news')->where('project', 1)->where('id', Input::get('id'))->first()
+        ];
     }
 ];
