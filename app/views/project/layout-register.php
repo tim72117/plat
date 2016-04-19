f90ea4d3e8fbd6fb0c3cd4352ad16564eb02625f
tim cai <tim72117@gmail.com>
2016-04-19 12:06:37 +0800

    dev change login and register layout

--- app/views/project/layout-register.php
+++ app/views/project/layout-register.php
@@ -1,23 +1,28 @@
 <!DOCTYPE html>
-<html xmlns:ng="http://angularjs.org" xml:lang="zh-TW" lang="zh-TW">
+<html xmlns:ng="http://angularjs.org" ng-app="app" xml:lang="zh-TW" lang="zh-TW">
 <head>
 <meta charset="utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <title><?=$project->name?></title>
 
 <!--[if lt IE 9]><script src="/js/html5shiv.js"></script><![endif]-->
-<script src="/js/jquery-1.11.2.min.js"></script>
-<script src="/js/angular/1.4.9/angular.min.js"></script>
-<script src="/css/Semantic-UI/2.1.8/components/transition.min.js"></script>
-<script src="/css/Semantic-UI/2.1.8/components/dropdown.min.js"></script>
+<script src="/js/angular/1.5.3/angular.min.js"></script>
+<script src="/js/angular/1.5.3/angular-sanitize.min.js"></script>
+<script src="/js/angular/1.5.3/angular-cookies.min.js"></script>
+<script src="/js/angular/1.5.3/angular-animate.min.js"></script>
+<script src="/js/angular/1.5.3/angular-aria.min.js"></script>
+<script src="/js/angular/1.5.3/angular-messages.min.js"></script>
+<script src="/js/angular_material/1.1.0/angular-material.min.js"></script>
 
 <link rel="stylesheet" href="/css/Semantic-UI/2.1.8/semantic.min.css" />
+<link rel="stylesheet" href="/js/angular_material/1.1.0/angular-material.min.css">
+
+<script>
+var app = angular.module('app', ['ngMaterial']);
+</script>
 </head>
 
-<body>
-
+<body ng-cloak layout="column">
     <?=$context?>
-    <?=$child_footer?>
-
 </body>
 </html>
--- app/views/project/use/auth/register.blade.php
+++ app/views/project/use/auth/register.blade.php
@@ -1,78 +1,74 @@
 
-<div class="ui basic segment">
-    <h1 class="ui center aligned grey header">申請臺灣後期中等教育整合資料庫查詢平台帳號</h1>
-    <h4 class="ui center aligned grey icon header">
-        <i class="puzzle icon"></i>
-        <div class="content">
-            申請步驟
-            <div class="sub header">請依下列步驟完成帳號申請</div>
-        </div>
-    </h4>
-</div>
+<md-toolbar layout="row">
+    <div class="md-toolbar-tools">
+        <h2>{{$project->name}}</h2>
+        <span flex></span>
+        <md-button href="/project/{{$project->code}}">登入</md-button>
+    </div>
+</md-toolbar>
 
-<div ng-cloak ng-app="app" ng-controller="register">
+<md-content ng-controller="register" layout="column" flex>
 
-    <div class="ui grid container">
+    <div layout="column" layout-align="center center" style="min-height:100px">
+        <h1 class="md-display-1">申請{{$project->name}}帳號</h1>
+    </div>
 
-        <div class="seven wide column">
-
-            <div class="ui fluid small vertical steps">
-                <div class="step" ng-class="{active: step == 'write'}">
-                    <i class="edit icon"></i>
-                    <div class="content">
-                        <div class="title">填寫申請表</div>
-                        <div class="description">請填完資料後點選申請表送出。</div>
+    <div layout="row" layout-align="center start">
+        <div layout="row" layout-sm="column" layout-xs="column" flex="60" flex-md="80" flex-sm="80" flex-xs="80">
+            <div flex="40" flex-sm="100" flex-xs="100">
+                <div class="ui fluid small vertical steps">
+                    <div class="step" ng-class="{active: step == 'write'}">
+                        <i class="edit icon"></i>
+                        <div class="content">
+                            <div class="title">填寫申請表</div>
+                            <div class="description">請填完資料後點選申請表送出。</div>
+                        </div>
                     </div>
-                </div>
-                <div class="step" ng-class="{active: step == 'print'}">
-                    <i class="print icon"></i>
-                    <div class="content">
-                        <div class="title">列印申請表</div>
-                        <div class="description">主管簽核後，將申請表正本寄給我們。</div>
+                    <div class="step" ng-class="{active: step == 'print'}">
+                        <i class="print icon"></i>
+                        <div class="content">
+                            <div class="title">列印申請表</div>
+                            <div class="description">主管簽核後，將申請表正本寄給我們。</div>
+                        </div>
                     </div>
-                </div>
-                <div class="step">
-                    <i class="mail icon"></i>
-                    <div class="content">
-                        <div class="title">更改密碼</div>
-                        <div class="description">到您註冊的信箱收取更改密碼的信件。</div>
+                    <div class="step">
+                        <i class="mail icon"></i>
+                        <div class="content">
+                            <div class="title">更改密碼</div>
+                            <div class="description">到您註冊的信箱收取更改密碼的信件。</div>
+                        </div>
                     </div>
-                </div>
-                <div class="step">
-                    <i class="checkmark box icon"></i>
-                    <div class="content">
-                        <div class="title">開通帳號</div>
-                        <div class="description">我們收到申請表後，即為您開通帳號。</div>
+                    <div class="step">
+                        <i class="checkmark box icon"></i>
+                        <div class="content">
+                            <div class="title">開通帳號</div>
+                            <div class="description">我們收到申請表後，即為您開通帳號。</div>
+                        </div>
                     </div>
                 </div>
             </div>
-
-        </div>
-
-        <div class="nine wide column">
-
-            <div ng-if="step == 'write'">
-                <form class="ui form segment top attached" ng-submit="saveRegister()" ng-class="{error: errors.length > 0}">
+            <div flex="60" flex-sm="100" flex-xs="100">
+                <form class="ui form segment top attached" ng-submit="saveRegister()" ng-class="{error: errors.length > 0}" ng-if="step == 'write'">
 
                     <div class="field">
                         <label>登入帳號 (e-mail)</label>
-                        <input type="email" ng-model="user.email" />
+                        <input type="email" required ng-model="user.email" />
                     </div>
 
                     <div class="two fields">
                         <div class="field">
                             <label>姓名</label>
-                            <input type="text" ng-model="user.username" />
+                            <input type="text" required ng-model="user.username" />
                         </div>
                         <div class="field">
                             <label>職稱</label>
-                            <input type="text" ng-model="user.contact.title" />
+                            <input type="text" required ng-model="user.contact.title" />
                         </div>
                     </div>
 
                     <div class="field">
                         <label>聯絡電話(服務單位)</label>
-                        <input type="tel" ng-model="user.contact.tel" placeholder="例：02-7734-3645#1234" />
+                        <input type="tel" required ng-model="user.contact.tel" placeholder="例：02-7734-3645#1234" />
                     </div>
 
                     <div class="field">
@@ -88,32 +84,38 @@
                     </div>
 
                     <div class="field">
-                        <label>單位所在縣市</label>
-                        <select ng-model="cityname" ng-options="city.cityname as city.cityname for city in citys" ng-change="getDepartments(cityname)">
-                            <option value="">選擇您服務的單位所在縣市</option>
-                        </select>
+                        <md-input-container>
+                            <label>選擇您服務的單位所在縣市</label>
+                            <md-select ng-model="cityname" style="min-width: 200px" ng-change="getDepartments(cityname)">
+                                <md-option ng-repeat="city in citys" value="@{{city.cityname}}">@{{city.cityname}}</md-option>
+                            </md-select>
+                        </md-input-container>
+                        <md-input-container>
+                            <label>選擇您服務的單位</label>
+                            <md-select ng-model="user.work.sch_id" style="min-width: 200px">
+                                <md-option ng-repeat="department in departments | filter:{type:user.work.type}" ng-value="department.id">@{{department.name}}</md-option>
+                            </md-select>
+                        </md-input-container>
                     </div>
 
                     <div class="field">
-                        <label>單位名稱</label>
-                        <select ng-model="user.work.sch_id" ng-options="department.id as department.id+' - '+department.name for department in departments | filter:{type:user.work.type}">
-                            <option value="">選擇您服務的單位</option>
-                        </select>
+                        <label>承辦業務</label>
+                        <md-checkbox ng-model="user.work.staff">學校人員調查(教師、校長調查)</md-checkbox>
+                        <md-checkbox ng-model="user.work.parent">家長調查</md-checkbox>
+                        <md-checkbox ng-model="user.work.student">學生調查</md-checkbox>
+                        <md-checkbox ng-model="user.work.tutor">導師調查</md-checkbox>
                     </div>
 
                     <div class="field">
                         <label>額外服務申請<span style="color:red">(需校長核准)</span></label>
-                        <div class="ui checkbox">
-                            <input type="checkbox" id="members[das]" ng-model="user.member.apply" class="hidden" />
-                            <label for="members[das]">線上分析系統</label>
-                        </div>
+                        <md-checkbox  ng-model="user.member.apply">
+                            線上分析系統
+                        </md-checkbox>
                     </div>
 
                     <div class="ui error message">
                         <div class="header">資料錯誤</div>
-                        <div class="ui horizontal list">
-                        <span class="item" ng-repeat="error in errors">@{{ error }}</span>
-                        </div>
+                        <p>@{{ errors.join('、') }}</p>
                     </div>
 
                     <div class="field">
@@ -124,36 +126,33 @@
 
                 </form>
 
+                <div ng-if="step == 'print'">
+                    <div class="ui top attached message">
+                        請開啟下列連結後，列印出申請單。
+                    </div>
+                    <div class="ui attached padded segment" style="min-height:300px">
+                        <i class="print icon"></i>
+                        <a target="_blank" href="/project/<?=$project->code?>/register/print/@{{ applying_id }}">列印申請單</a>
+                    </div>
+                </div>
+
                 <div class="ui bottom attached warning message">
                     @include('project.auth-login-bottom')
                 </div>
-            </div>
 
-            <div ng-if="step == 'print'">
-                <div class="ui top attached message">
-                    請開啟下列連結後，列印出申請單。
-                </div>
-                <div class="ui attached padded segment">
-                    <i class="print icon"></i>
-                    <a target="_blank" href="/project/<?=$project->code?>/register/print/@{{ applying_id }}">列印申請單</a>
-                </div>
-                <div class="ui bottom attached warning message">
-                    @include('project.auth-login-bottom')
-                </div>
             </div>
-
         </div>
-
     </div>
 
-</div>
+</md-content>
 
 <script>
-angular.module('app', [])
-.constant("CSRF_TOKEN", '{{ csrf_token() }}')
+app.constant("CSRF_TOKEN", '{{ csrf_token() }}')
+
 .config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 }])
+
 .controller('register', function($scope, $http, CSRF_TOKEN) {
     $scope.step = 'write';
     $scope.user = {work: {}, contact: {}, member: {project_id: 4, apply: false}};
