c733ff9cab9484a0381925a18ec069d6ab83276f
ken <momo60104@hotmail.com>
2016-08-19 11:25:46 +0800

    release/v1.4: adding attach files

--- app/views/project/tted/auth/login.blade.php
+++ app/views/project/tted/auth/login.blade.php
@@ -29,6 +29,10 @@
                     </md-card-title-text>
                 </md-card-title>
                 <md-card-content>
+                    <p ng-repeat="file in post.files">
+                        <i class="attach icon red"></i>
+                        <a href="/api/news/download/@{{file.pivot.id}}">@{{ file.title }}</a>
+                    </p>
                     <p>@{{post.context}}</p>
                 </md-card-content>
             </md-card>
