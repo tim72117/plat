6f121d3ebeeaae8aa5858f36f882e67c32de136e
tim <tim72117@gmail.com>
2016-10-26 11:54:42 +0800

    dev: tiped register

--- app/views/project/tiped/auth/register.blade.php
+++ app/views/project/tiped/auth/register.blade.php
@@ -7,7 +7,7 @@
     </div>
 </md-toolbar>
 
-<md-content layout="column" flex>
+<md-content layout="column" flex ng-controller="registerController">
 
     <div layout="column" layout-align="center center" style="min-height:100px">
         <h1 class="md-display-1">申請{{$project->name}}帳號</h1>
@@ -15,99 +15,95 @@
 
     <div layout="column" layout-align="start center">
         <div style="max-width: 500px">
-            <?=Form::open(array(
-                'url' => 'project/' . Request::segment(2) . '/register/save',
-                'method' => 'post',
-                'class' => 'ui form segment attached' . ($errors->isEmpty() ? '' : ' error'),
-                'name' => 'registerForm'))?>
+            <form ng-if="step == 1" class="ui form segment attached" action="register/save" method="post" ng-submit="save($event)" ng-class="{error: errors.length > 0}">
 
                 <div class="field">
                     <label>登入帳號 (e-mail)</label>
-                    <?=Form::text('email', '', array())?>
+                    <input type="email" required name="email" ng-model="user.email" />
                 </div>
 
                 <div class="two fields">
                     <div class="field">
                         <label>姓名</label>
-                        <?=Form::text('name', '', array())?>
+                        <input type="text" required ng-model="user.username" />
                     </div>
                     <div class="field">
                         <label>職稱</label>
-                        <?=Form::text('title', '', array())?>
+                        <input type="text" required ng-model="user.contact.title" />
                     </div>
                 </div>
 
-                <div class="field">
-                    <label>聯絡電話(服務單位)</label>
-                    <?=Form::text('tel', '', array())?>
-                </div>
-
-                <div class="field">
-                    <label>服務單位</label>
-                    <?=Form::select('sch_id', [
-                            ''     => '選擇您服務的單位',
-                            '0016' => '國立陽明大學',
-                            '1028' => '臺北醫學大學',
-                            '9999' => '其他'
-                        ],
-                        Input::old('sch_id'),
-                        ['ng-model' => 'sch_id', 'ng-init' => 'sch_id=\'' . Input::old('sch_id') . '\''])?>
-                </div>
-                <div class="field" ng-if="sch_id=='9999'">
-                    <label>單位名稱</label>
-                    <?=Form::text('sch_name', '', array())?>
-                </div>
-                <div class="field" ng-if="sch_id=='1028'">
-                    <?=Form::select('dep_id', [
-                        ''       => '選擇您服務系所',
-                        '000000' => '校級承辦人',
-                        '229903' => '醫學人文研究所',
-                        '340901' => '醫務管理學系',
-                        '380215' => '醫療暨生物科技法律研究所',
-                        '521107' => '生醫材料暨(組織)工程研究所',
-                        '720101' => '醫學系',
-                        '720102' => '臨床醫學研究所',
-                        '720117' => '醫學科學系',
-                        '720131' => '轉譯醫學學位學程',
-                        '720135' => '神經再生醫學學位學程',
-                        '720201' => '公共衛生學系',
-                        '720213' => '傷害防治學研究所',
-                        '720224' => '全球衛生暨發展學位學程',
-                        '720301' => '藥學系',
-                        '720309' => '生(物)藥(科)學系',
-                        '720323' => '癌症生物(學)與藥物研發學位學程',
-                        '720328' => '臨床藥物基汍樴[蛋白質體學學位學程',
-                        '720329' => '中草藥臨床藥物研發學位學程',
-                        '720504' => '保健營養(技術)學系',
-                        '720601' => '護理學系',
-                        '720607' => '長期照護系',
-                        '720613' => '呼吸治療學系',
-                        '720708' => '醫學檢驗(暨)生物技術學系',
-                        '720714' => '醫學資訊學系',
-                        '720801' => '牙醫(科學)學系',
-                        '720803' => '牙體技術(暨材料)系',
-                        '720804' => '口腔衛生(科學)學系',
-                        '729988' => '醫藥衛生類產業研發專班',
-                        '760219' => '高齡健康管理學系'
-                    ], '')?>
+                <div class="two fields">
+                    <div class="field">
+                        <label>服務單位名稱</label>
+                        <input type="text" required ng-model="user.contact.department" />
+                    </div>
+                    <div class="field">
+                        <label>聯絡電話(服務單位)</label>
+                        <input type="tel" required ng-model="user.contact.tel" placeholder="例：02-7734-3645#0000" />
+                    </div>
                 </div>
 
                 <div class="ui error message">
                     <div class="header">資料錯誤</div>
-                    <p><?=implode('、', array_filter($errors->all()))?></p>
+                    <div class="ui horizontal list">
+                    <span class="item" ng-repeat="error in errors">@{{ error }}</span>
+                    </div>
                 </div>
 
                 <div class="field">
                     <label>一旦點擊註冊，即表示你同意 <a href="register/terms" target="_blank">使用條款</a>。</label>
                 </div>
 
-                <div class="ui submit positive button" onclick="registerForm.submit()">註冊</div>
-            <?=Form::close()?>
+                <button type="submit" class="ui submit positive button" ng-class="{loading: saving}">註冊</button>
+            </form>
+
+            <div ng-if="step == 3">
+                <div class="ui top attached message">
+                    請開啟下列連結後，列印出申請單。
+                </div>
+                <div class="ui attached padded segment">
+                    <i class="print icon"></i>
+                    <a target="_blank" href="/project/<?=$project->code?>/register/print/@{{ applying_id }}">列印申請單</a>
+                </div>
+            </div>
 
             <div class="ui bottom attached warning message">
                 @include('project.auth-login-bottom')
             </div>
+
         </div>
     </div>
 
 </md-content>
+
+<script>
+app.constant("CSRF_TOKEN", '{{ csrf_token() }}')
+
+.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+}])
+
+.controller('registerController', function($scope, $http, CSRF_TOKEN, $filter) {
+    $scope.step = 1;
+    $scope.user = {work: {}, contact: {}};
+
+    $scope.save = function(event) {
+        event.preventDefault();
+        $scope.saving = true;
+        $http({method: 'POST', url: 'register/save', data:{'_token': CSRF_TOKEN, user: $scope.user}})
+        .success(function(data, status, headers, config) {
+            $scope.errors = data.errors;
+            if (data.applying_id) {
+                $scope.step = 3;
+                $scope.applying_id = data.applying_id;
+            };
+            $scope.saving = false;
+        })
+        .error(function(e) {
+            console.log(e);
+        });
+    };
+
+});
+</script>
--- app/views/project/tiped/auth/register_validator.php
+++ app/views/project/tiped/auth/register_validator.php
@@ -1,36 +1,31 @@
 <?php
 
-$input = Input::only('email', 'name', 'title', 'tel', 'sch_id', 'dep_id', 'sch_name');
+$input = [
+    'email'      => Input::get('user.email'),
+    'username'   => Input::get('user.username'),
+    'title'      => Input::get('user.contact.title'),
+    'tel'        => Input::get('user.contact.tel'),
+    'department' => Input::get('user.contact.department'),
+];
 
 $rulls = array(
-    'email'               => 'required|email|unique:users',
-    'name'                => 'required|max:10',
-    'title'               => 'required|max:10',
-    'tel'                 => 'required|max:20',
-    'sch_id'              => 'required|alpha_num|max:4',
-    'sch_name'            => 'required_if:sch_id,9999|max:30',
-    'dep_id'              => 'required_if:sch_id,1028|alpha_num|max:6',
+    'email'      => 'required|email|unique:users,email',
+    'username'   => 'required',
+    'title'      => 'required',
+    'tel'        => 'required',
+    'department' => 'required',
 );
 
 $message = array(
-    'email.required'       => '電子郵件必填',
-    'name.required'        => '姓名必填',
-    'title.required'       => '職稱必填',
-    'tel.required'         => '連絡電話必填',
-    'sch_id.required'      => '服務單位必填',
-    'sch_name.required_if' => '服務單位名稱必填',
-    'dep_id.required_if'   => '服務系所必填',
+    'email.required'        => '電子郵件必填',
+    'email.email'           => '電子郵件格式錯誤',
+    'email.unique'          => '電子郵件已被註冊',
 
-    'email.email'            => '電子郵件格式錯誤',
-    'email.unique'           => '電子郵件已被註冊',
-    'name.max'               => '姓名最多10個字',
-    'title.max'              => '職稱最多10個字',
-    'tel.max'                => '連絡電話最多20個字',
-    'sch_id.alpha_num'       => '服務單位格式錯誤',
-    'sch_id.max'             => '服務單位格式錯誤',
-    'sch_name.max'           => '服務單位最多30個字',
-    'dep_id.alpha_num'       => '服務系所格式錯誤',
-    'dep_id.max'             => '服務系所格式錯誤',
+    'username.required'     => '姓名必填',
+    'title.required'        => '職稱必填',
+    'tel.required'          => '連絡電話必填',
+    'department.required'   => '服務單位名稱必填',
+
 );
 
 return Validator::make($input, $rulls, $message);
