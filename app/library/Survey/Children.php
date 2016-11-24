d1f39d8d89657da7ccdbe877fa2b56493a8731c7
tim <tim72117@gmail.com>
2016-11-24 11:28:58 +0800

    feature/survey: move Node function to Children

--- app/library/Survey/Children.php
+++ app/library/Survey/Children.php
@@ -52,4 +52,36 @@ trait Children {
         $rule->questions()->attach([$question->id]);
     }
 
+    public function rules()
+    {
+        $rule = $this->node->childrenRule;
+
+        return $rule ? $rule->node : \Illuminate\Database\Eloquent\Collection::make([]);
+    }
+
+    public function getParent()
+    {
+        if ($this->node->byRules->isEmpty()) {
+            return Book::make($this->book);
+        }
+
+        $children = $this->node->byRules->first(function($index, $rule) {
+            return $rule->is->expression == 'children';
+        });
+
+        $parameter = $children->is->parameters->first();
+
+        switch ($parameter->type) {
+            case 'answer':
+                $parent = Answer::find($parameter->answer);
+                break;
+
+            case 'question':
+                $parent = Question::find($parameter->question);
+                break;
+        }
+
+        return $parent;
+    }
+
 }
