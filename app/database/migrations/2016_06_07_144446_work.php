92e527509443c43c3abc33db5b8bb410f5a980af
tim <tim72117@gmail.com>
2016-11-24 16:03:14 +0800

    release/v1.4.8: row_table row_sheet, work migrations

--- app/database/migrations/2016_06_07_144446_work.php
+++ app/database/migrations/2016_06_07_144446_work.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class WorkUsed extends Migration {
+class Work extends Migration {
 
     /**
      * Run the migrations.
@@ -12,12 +12,10 @@ class WorkUsed extends Migration {
      */
     public function up()
     {
-        Schema::create('work_used', function (Blueprint $table) {
+        Schema::create('works', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
-            $table->string('sch_id', 6);          
-            $table->enum('department_class', [1, 0]);
-            $table->timestamps();
+            $table->integer('member_id');
+            $table->integer('organization_id');
         });
     }
 
@@ -28,7 +26,7 @@ class WorkUsed extends Migration {
      */
     public function down()
     {
-        Schema::drop('work_used');
+        Schema::drop('works');
     }
 
 }
---
+++ app/database/migrations/2016_10_30_162559_create_row_sheets.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRowSheets extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('row_sheets', function (Blueprint $table) {
+            $table->increments('id');
+			$table->integer('file_id');
+            $table->string('title', 50);
+            $table->boolean('editable');
+            $table->boolean('fillable');
+			$table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('row_sheets');
+	}
+
+}
