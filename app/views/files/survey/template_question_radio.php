6bab6cf0dee5ef33067ae6a976636f592d5c6dcb
ken <momo60104@hotmail.com>
2017-02-14 17:20:36 +0800

    feature/survey: fix title of radio shown bug

--- app/views/files/survey/template_question_radio.php
+++ app/views/files/survey/template_question_radio.php
@@ -1,6 +1,7 @@
 <md-radio-group ng-model="answer" ng-disabled="node.saving" survey-input parent="answer" ng-repeat="question in node.questions" ng-change="saveAnswer(answer, answer.value)">
+	<label>{{question.title}}</label><br><br>
     <md-radio-button ng-repeat="answer in node.answers" ng-value="answer" class="md-primary">{{answer.title}}</md-radio-button>
     <div style="padding-left: 5px">
         <survey-node ng-repeat="children in question.childrens" node="children"></survey-node>
-    <div>
+    </div>
 </md-radio-group>
