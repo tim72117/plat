e73b024c3a2641bf4e9a85653f1ab05714eeae2f
tim <tim72117@gmail.com>
2017-03-09 15:40:42 +0800

    feature/survey: SurveyRepository

--- app/library/Survey/SurveyRepository.php
+++ app/library/Survey/SurveyRepository.php
@@ -2,8 +2,20 @@
 
 namespace Plat\Survey;
 
-interface SurveyRepositoryInterface
+use DB;
+
+class SurveyRepository implements SurveyRepositoryInterface
 {
+    function __construct($book_id)
+    {
+        $this->book_id = $book_id;
+    }
+
+    public static function create($book_id)
+    {
+        return new self($book_id);
+    }
+
     /**
      * Increment a row in the repository.
      *
@@ -11,7 +23,12 @@ interface SurveyRepositoryInterface
      * @param  array   $default
      * @return int|bool
      */
-    public function increment($id, $default = []);
+    public function increment($id, $default = [])
+    {
+        $attributes = array_add($default, 'created_by', $id);
+
+        DB::table($this->book_id)->insert($attributes);
+    }
 
     /**
      * Decrement a row in the repository.
@@ -19,7 +36,10 @@ interface SurveyRepositoryInterface
      * @param  string  $id
      * @return int|bool
      */
-    public function decrement($id);
+    public function decrement($id)
+    {
+
+    }
 
     /**
      * Retrieve a value from the repository by id in the repository.
@@ -28,7 +48,10 @@ interface SurveyRepositoryInterface
      * @param  string  $key
      * @return mixed
      */
-    public function get($id, $key);
+    public function get($id, $key)
+    {
+
+    }
 
     /**
      * Store an value in the repository.
@@ -38,19 +61,28 @@ interface SurveyRepositoryInterface
      * @param  string     $value
      * @return void
      */
-    public function put($id, $key, $value);
+    public function put($id, $key, $value)
+    {
+
+    }
 
     /**
      * Get all values of row by id in the repository.
      *
      * @return array
      */
-    public function all($id);
+    public function all($id)
+    {
+
+    }
 
     /**
      * Determine if row exists in the repository.
      *
      * @return array
      */
-    public function exist($id);
+    public function exist($id)
+    {
+        return false;
+    }
 }
