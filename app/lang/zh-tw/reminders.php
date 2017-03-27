a67d12bf78f43482046682a89223aa21b12dd52a
tim <tim72117@gmail.com>
2017-03-27 14:20:06 +0800

    master: remove vendor folder

---
+++ app/lang/zh-tw/reminders.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    "password" => "密碼格式錯誤",
+
+    "user"     => "電子郵件信箱錯誤",
+
+    "token"    => "此連結已經過期，請點選忘記密碼重新寄送連結。",
+
+);
